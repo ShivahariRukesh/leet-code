f23d5f31e1969bf9aa2a47c0bf1b8b207083a67b
ShivahariRukesh <478gnujnoej@gmail.com>
2024-09-28 02:02:33 +0545

    leetcode 12 complete

--- _12-IntegerToRoman/IntegerToRoman.py
+++ _12-IntegerToRoman/IntegerToRoman.py
@@ -1,28 +1,66 @@
-num= 3800
-s = str(num)
-res=""
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        s = str(num)
+        res=""
 
-for i in range(len(s)):
-    if(len(s)==4):
-        c=0
-        while(c<int(s[0])):
-            res=res+'M'
-            c=c+1
-    elif(len(s)==3):
-        if(int(s[0])<4):
-            c=0
-            while(c<int(s[0])):
-                res=res+'C'
-                c=c+1
-        elif(int(s[0])==4):
-            res=res+'CD'
-        elif(int(s[0])==9):
-            res=res+'CM'
-        else:
-            c=5
-            res=res+'D'
-            while(c<int(s[0])):
-                res=res+'C'
-                c=c+1            
-    s=s[i+1:len(s)]
-print(res)            
+        for i in range(len(s)):
+            if(len(s)-i==4):
+                c=0
+                while(c<int(s[i])):
+                    res=res+'M'
+                    c=c+1
+            elif(len(s)-i==3):
+                if(int(s[i])<4):
+                    c=0
+                    while(c<int(s[i])):
+                        res=res+'C'
+                        c=c+1
+                elif(int(s[i])==4):
+                    res=res+'CD'
+                elif(int(s[i])==9):
+                    res=res+'CM'
+                else:
+                    c=5
+                    res=res+'D'
+                    while(c<int(s[i])):
+                        res=res+'C'
+                        c=c+1    
+            elif(len(s)-i==2):
+                if(int(s[i])<4):
+                    c=0
+                    while(c<int(s[i])):
+                        res=res+'X'
+                        c=c+1
+                elif(int(s[i])==4):
+                    res=res+'XL'
+                elif(int(s[i])==9):
+                    res=res+'XC'
+                else:
+                    c=5
+                    res=res+'L'
+                    while(c<int(s[i])):
+                        res=res+'X'
+                        c=c+1  
+            elif(len(s)-i==1):
+                if(int(s[i])<4):
+                    c=0
+                    while(c<int(s[i])):
+                        res=res+'I'
+                        c=c+1
+                elif(int(s[i])==4):
+                    res=res+'IV'
+                elif(int(s[i])==9):
+                    res=res+'IX'
+                else:
+                    c=5
+                    res=res+'V'
+                    while(c<int(s[i])):
+                        res=res+'I'
+                        c=c+1     
+
+            
+        return res
