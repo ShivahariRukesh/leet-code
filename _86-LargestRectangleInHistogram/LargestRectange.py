c7f4101ea0291896d5d1f41a18552800026dccea
ShivahariRukesh <478gnujnoej@gmail.com>
2024-09-30 11:04:23 +0545

    leetcode 86 myownCompleted

--- _86-LargestRectangleInHistogram/LargestRectange.py
+++ _86-LargestRectangleInHistogram/LargestRectange.py
@@ -12,18 +12,23 @@ class Solution(object):
         """
         if(len(heights)==1):
             return heights[0]
-        max=0
         
+        max=heights[0]
         for i in range(len(heights)-1):
             min =heights[i]
+            
             for j in range(i+1,len(heights)):
+                max=self.maxiNum(heights[j],max)
                 if(heights[i]==0 and heights[j]==0):
-                    max=0
+                    min = 0
+                    max=self.maxiNum(0,max)
                 elif(heights[i]==0 and heights[j]!=0):
-                    max=heights[j]
+                    min = 0
+                    max=self.maxiNum(heights[j],max)
 
                 elif(heights[i]!=0 and heights[j]==0):
-                    max=heights[i]
+                    min=0
+                    max=self.maxiNum(heights[i],max)
                 else:
                     min = self.miniNum(min,heights[j])
                     max=self.maxiNum(max, min*(j-i+1))
