cb40cd0dd59c7f3b6f8572328a50e7b7f58d2caa
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-01 12:19:28 +0545

    leetcode 84 using monostack concept

--- _86-LargestRectangleInHistogram/LargestRectange.py
+++ _86-LargestRectangleInHistogram/LargestRectange.py
@@ -1,4 +1,6 @@
-class Solution(object):
+
+# My Own -Time Limit Exceeded 
+class AnotherSolution(object):
 
     def maxiNum(self,num1,num2):
         return  num1 if(num1>num2 ) else num2
@@ -34,3 +36,49 @@ class Solution(object):
                     max=self.maxiNum(max, min*(j-i+1))
                     
         return max
+    
+    # Time Limit Exceeded
+    
+    
+    
+    # The one submitted on leetcode using concept of monostack
+    
+    
+class Solution(object):
+
+    def maxiNum(self,num1,num2):
+        return  num1 if(num1>num2 ) else num2
+
+
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        max=0
+        stack=[]
+        stack.append({"index":0,"height":heights[0]})
+        for i in range(1,len(heights)):
+            c=-1
+
+            while((len(stack)!=0) and (heights[i]<stack[-1]["height"])):
+                newIndex = stack[-1]["index"]
+                max = self.maxiNum(stack[-1]["height"]*(i-newIndex),max)
+                stack.pop()
+                c=0
+            if(c==0):
+                stack.append({"index":newIndex,"height":heights[i]})
+            else:
+                stack.append({"index":i,"height":heights[i]})
+
+        
+        while(len(stack)!=0):
+            max =  self.maxiNum(max, stack[-1]["height"]*(len(heights)-stack[-1]["index"]) )
+            stack.pop()
+
+
+
+
+        print(max)
+                    
+        return max 
