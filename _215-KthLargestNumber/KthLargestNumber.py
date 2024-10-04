fbd976ff21b0b7cdbc5023a6e48177ae5fc04640
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-04 17:18:26 +0545

    leetcode 215 by using max heap concept

--- _215-KthLargestNumber/KthLargestNumber.py
+++ _215-KthLargestNumber/KthLargestNumber.py
@@ -16,6 +16,40 @@ class Solution(object):
 
         return arr[-1]
     
+
+
+# Using max-heap to eradicate the exceeding time limit    
+arr =[1,-7,3,2,6,0,-8]
+sort =[]
+
+
+def heapae(arr):
+    for i in range(len(arr)):
+        c=i+1
+        while(c>0):
+          if(arr[c-1]<arr[i]):
+              temp = arr[c-1]
+              arr[c-1]=arr[i]
+              arr[i]=temp
+              break
+          c=c//2
+    return arr
+       
+arr =heapae(arr)
+
+while(arr and len(arr)):
+    temp = arr[0]
+    arr[0] = arr[-1]
+    arr[-1]=temp
+    
+    sort.append(arr.pop())
+    
+    heapae(arr)
+   
+print(sort)   
+
+# Using max heap sort
+
     
 n =int(input("Enter the length of an array\t"))
 arr=[]
