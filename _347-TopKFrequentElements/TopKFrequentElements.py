1b69e23b5a4c6778ea30e7b221995af9d0941a2f
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-08 14:27:10 +0545

    leetcode 347 completed

--- _347-TopKFrequentElements/TopKFrequentElements.py
+++ _347-TopKFrequentElements/TopKFrequentElements.py
@@ -15,22 +15,29 @@ class Solution(object):
 
         for i in range(len(nums)):
         
-            if c<nums[i] or :
+            if c<nums[i]:
                 arr.append([c,count])
                 c=nums[i]
-                count =0
-                continue
+                count =1
+                
             else:
                 count =count+1
-        print(arr)
+        arr.append([c,count])
         def sortMe(val):
             return val[1]
         arr.sort(key=sortMe,reverse=True)
 
         for i in range(0,k):
             arr[i]= arr[i][0]
-        return arr
+
+        return arr[0:k]
 
 
+arr = []
 
-        
+n = int(input(f"Enter the size of an array\t"))
+for i in range(n):
+    arr.append(int(input(f"Enter the number {i+1}\t")))
+k = int(input(f"Enter the Kth value(it must be small or equals to the unique numbers in an array\t"))
+
+print("The most frequent repeated top K numbers are\t",Solution().topKFrequent(arr,k))
