d621f6cf1b2fdcab7c73f24c65b791ee0e8733df
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-15 23:58:20 +0545

    Renamed file, Integrated Input Section, Added Readme

--- _2997-MinNumOfOperations../MinimumNumOfOperationsToMakeArrayXOREqualToK.py
+++ _2997-MinNumOfOperations../MinimumNumOfOperationsToMakeArrayXOREqualToK.py
@@ -14,8 +14,18 @@ class Solution(object):
             rem2 = k%2
             if(rem1!=rem2):
                 count=count+1
-            sol=sol/2
-            k=k/2
-        
+            sol=sol//2
+            k=k//2
+        print(count)    
         return count
+    
+    
+    
+arr1 = []
+n = int(input("Enter the length for an array\t"))
+for i in range(n):
+    arr1.append(int(input(f"Enter the value for {i+1} position\t")))
+k = int(input("Enter the value for k\t"))
+
+print("The minimum number of operations to make array XOR equal to K is\t",Solution().minOperations(arr1,k))    
             
