064cafda9915785d38764e8b35971c80f8fd8e7a
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-21 13:32:33 +0545

    leetcode 153 completed

--- _153-FindMinimumInRotatedSortedArray/FIndMinimum.py
+++ _153-FindMinimumInRotatedSortedArray/FIndMinimum.py
@@ -1,18 +1,43 @@
-#Completed in O(n) time
-
 class Solution(object):
     def findMin(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        i=0
-        while(i+1<len(nums) and nums[i]<nums[i+1]):
-            i=i+1
-        
-        if(i==len(nums)-1):
-            return nums[0]
-        else:
-            return nums[i+1]
 
-#
+        f,l=0,len(nums)-1
+       
+        if(nums[f]<nums[l]):
+            min= nums[0]
+        else:
+            min = nums[l]
+
+        while(f<=l):
+            mid = (f+l)//2
+ 
+            if(min>nums[mid]):
+                min = nums[mid]
+            if(nums[f]<nums[mid]):
+                f= mid+1
+                if(min>nums[f]):
+                    min = nums[f]
+        
+            else:
+                l = mid-1
+                if(min>nums[l]):
+                    min= nums[l]
+                  
+
+        return min
+
+
+arr1=[]
+n = int(input("Enter the length of the list\t"))
+
+print("Please enter the rotated sorted list\n")
+for i in range(n):
+    arr1.append(int(input(f"Enter the {i+1}th element\t")))
+
+
+    
+print("The smallest number from the given array is\t",Solution().findMin(arr1))
